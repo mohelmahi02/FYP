5f87aa421f769d0c894f2ca07b4dca38ca3088b9
mohelmahi02 <123365009+mohelmahi02@users.noreply.github.com>
2026-03-03 14:19:58 +0000

    hardcode database url for testing

--- services/db_service.py
+++ services/db_service.py
@@ -11,7 +11,8 @@ def get_conn():
        
         if database_url.startswith('postgres://'):
             database_url = database_url.replace('postgres://', 'postgresql://', 1)
-        return psycopg2.connect(database_url)
+        
+       # return psycopg2.connect(database_url)
     else:
         
         return psycopg2.connect(
